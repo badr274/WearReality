545ba888e41112ff11e27dc7789fa8d48ba99d69
YomnaEmara <yomnaemara24@gmail.com>
2025-08-04 02:25:38 +0300

    card page

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import Categories from "../../components/home/Categories";
+import Card from "../../components/Card";
 import "./home.css";
 const Home = () => {
   return (
     <div>
       <Categories />
+      <Card />
     </div>
   );
 };
