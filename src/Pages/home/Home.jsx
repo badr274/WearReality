df532a268fc4c8b1bc7212eae861c655b0a09aab
badr274 <145036775+badr274@users.noreply.github.com>
2025-08-03 22:46:19 +0300

    end categories slider

---
+++ src/Pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import Categories from "../../components/home/Categories";
+import "./home.css";
+const Home = () => {
+  return (
+    <div>
+      <Categories />
+    </div>
+  );
+};
+
+export default Home;
