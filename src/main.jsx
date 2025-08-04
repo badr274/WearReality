106f81d616922231009c8f7c129669d7d40dd5a9
badr274 <145036775+badr274@users.noreply.github.com>
2025-08-04 14:31:29 +0300

    adding css variables

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import CartProvider from "./context/CartContext.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </BrowserRouter>
 );
