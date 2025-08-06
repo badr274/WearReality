eafc8655a77a9c6808e72d19d044ce2b49a6f8a5
salmamohamed01 <smsmreem2004@gmail.com>
2025-08-06 11:51:51 +0300

    Customer

--- src/Pages/Admin/Customer.jsx
+++ src/Pages/Admin/Customer.jsx
@@ -3,7 +3,54 @@ import axios from 'axios';
 import { Container, Row, Col, Card, Spinner, Badge } from 'react-bootstrap';
 
 const Customers = () => {
- 
+ const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('https://ecommerce.routemisr.com/api/v1/users');
+        const fetchedUsers = response.data.users.slice(0, 40);
+        setUsers(fetchedUsers);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <Container className="mt-4">
+      <h2 className="fw-bold  mb-4" style={{ color: 'rgb(101, 18, 20)' }}>Customers</h2>
+
+      {loading ? (
+        <div className="text-center">
+          <Spinner animation="border" variant="primary" />
+        </div>
+      ) : (
+        <>
+          {/* Table Headers */}
+          <Row className="mb-3 fw-bold ms-5" style={{ color: 'rgb(101, 18, 20)' }}>
+            <Col md={4}>Name</Col>
+            <Col md={4}>Email</Col>
+            <Col md={4}>Registered</Col>
+          </Row>
+
+          {/* Customer Data */}
+          {users.map(user => (
+            <Row key={user._id} className="align-items-center mb-3 ms-5">
+              <Col md={4}>{user.name}</Col>
+              <Col md={4}>{user.email}</Col>
+              <Col md={4}>{new Date(user.createdAt).toLocaleDateString()}</Col>
+            </Row>
+          ))}
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default Customers;
---
+++ src/Pages/Foulder/Contact.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Contact.css";
+import { FaEnvelope, FaUser } from "react-icons/fa";
+
+const Contact = () => {
+  return (
+    <div className="contact-container" id="Contact">
+      <div className="contact-box">
+        <h2 className="contact-title">Get in touch</h2>
+        <p className="contact-subtitle">
+          Need assistance? Our team is just a message away.
+        </p>
+
+        <form className="contact-form">
+          <label className="contact-label"> EMAIL *</label>
+          <div className="input-wrapper">
+            <FaEnvelope className="icon" />
+            <input type="email"placeholder="Ex. yourname@gmail.com"required/>
+          </div>
+
+          <label className="contact-label">YOUR NAME</label>
+          <div className="input-wrapper">
+            <FaUser className="icon" />
+            <input type="text" placeholder="First Name" required />
+          </div>
+
+          <div className="input-wrapper">
+            <FaUser className="icon" />
+            <input type="text" placeholder="Last Name" required />
+          </div>
+
+          <button type="submit" className="next-button">
+            Next
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
